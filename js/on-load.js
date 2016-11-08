833254b4c1b360027e7274bebda4594b605ece11
Kasatkin Sergey <kasatkin87@i.ua>
2016-11-08 15:24:40 +0200

    Закончил блок "Статьи", начал "Футтер"

---
+++ js/on-load.js
@@ -0,0 +1,11 @@
+window.onload = function (){
+	var scrollUp = document.getElementById('back-top'); // ����� �������
+	// show button scroll-up
+	window.onscroll = function () { // ��� ������� ���������� � ������� ����
+		if ( window.pageYOffset > 0 ) {
+		scrollUp.style.display = 'block';
+		} else {
+		scrollUp.style.display = 'none';
+		}
+	};
+}
