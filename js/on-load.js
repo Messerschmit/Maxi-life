47bc65e4fbd0b5f8879f93fa8844c5482c45fc3f
Kasatkin Sergey <kasatkin87@i.ua>
2016-11-16 22:42:01 +0200

    Добавлены скрипты для отправки почты

--- js/on-load.js
+++ js/on-load.js
@@ -1,7 +1,7 @@
 window.onload = function (){
-	var scrollUp = document.getElementById('back-top'); // ����� �������
+	var scrollUp = document.getElementById('back-top');
 	// show button scroll-up
-	window.onscroll = function () { // ��� ������� ���������� � ������� ����
+	window.onscroll = function () {
 		if ( window.pageYOffset > 0 ) {
 		scrollUp.style.display = 'block';
 		} else {
